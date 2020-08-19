977677de8f9c5ecb583644cfe8a4d4512979c35a
taiba <bukaritaiba20@gmail.com>
2020-08-19 06:54:59 -0700

    My first express-starter

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const port = 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello express JS");
+})
+
+app.listen(port, () => {
+    console.log('server is running....');
+})
+
+
+
+
+
+
+
+
+
+
+
+
